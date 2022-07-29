68790b41e1d6505a36ba7e45e5c848cd6ff3dccf
mailin <mailinadaromdp@gmail.com>
2022-07-29 19:46:48 -0300

    Ejercicios Resueltos

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,20 +1,28 @@
 // Do not change any of the function names
 
+const { cuentoElementos } = require("../../04-JS-III/homework/homework");
+
 function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var primerLetra = nombre[0].toUpperCase();
+  var resto = nombre.slice(1);
+  return primerLetra + resto;
+  
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,25 +30,45 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var sumar = 0;
+  for(let i = 0; i < numeros.length; i++){
+    sumar = sumar + numeros[i];
+  };
+  cb(sumar);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(cb);
 }
 
+//REVISAR: como lograr que tengas la misma longitud (condicional??)
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var nuevoArray = array.map(cb);
+  return nuevoArray;
+  
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+
+  var nuevoArray = [];
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i].startsWith('a')){
+      nuevoArray.push(array[i]);
+    }
+  }
+  
+  return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea
